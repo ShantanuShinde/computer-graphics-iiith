33e3efc615db30872480c4ea0bc35a4ffefe796c
Shantanu Shinde <shinde.shantanu21@gmail.com>
2019-05-31 20:57:39 +0530

    added changing starting position feature and small part of the manual

--- SRIP/Codes/js/main_script.js
+++ SRIP/Codes/js/main_script.js
@@ -1,11 +1,9 @@
-
-
  var canvas = document.getElementById("renderCanvas");
  var engine = new BABYLON.Engine(canvas,true);
 
 var scene, camera, sphere;
 var Xaxis, Yaxis, Zaxis;
-var vx,vy,vz;
+var vx,vy,vz, px, py, pz;;
 var transformCo_ordinate;
 var textplaneTexture; 
 
@@ -18,7 +16,7 @@ function init(){
 
     // Create camera
     camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 100, new BABYLON.Vector3(0,0,0), scene);
-    camera.setPosition(new BABYLON.Vector3(100,100,100))
+    camera.setPosition(new BABYLON.Vector3(300,300,300))
     
     camera.attachControl(canvas,true);
     
@@ -37,23 +35,23 @@ function init(){
 
 
     // Create sphere
-    sphere = new BABYLON.MeshBuilder.CreateSphere("sphere", {diameter:3}, scene);
+    sphere = new BABYLON.MeshBuilder.CreateSphere("sphere", {diameter:5}, scene);
     var material = new BABYLON.StandardMaterial(scene);
     material.alpha = 1;
     material.diffuseColor = new BABYLON.Color3(1,1,0);
     sphere.material = material;
 
     // Display sphere coordinates
-    var textPlane = BABYLON.MeshBuilder.CreatePlane("textPlane",{width:50,height:10,sideOrientation:2},scene);
+    var textPlane = BABYLON.MeshBuilder.CreatePlane("textPlane",{width:150,height:20,sideOrientation:2},scene);
     textPlane.setParent(sphere);
-    textPlane.position.y = 7;
+    textPlane.position.y = 20;
     textPlane.billboardMode = BABYLON.Mesh.BILLBOARDMODE_ALL;
     var material = new BABYLON.StandardMaterial("textPlaneMaterial", scene);
     textplaneTexture = new BABYLON.DynamicTexture("dynamic texture", {width:512,height:256}, scene);
     material.diffuseTexture = textplaneTexture;
     textPlane.material = material;
     textplaneTexture.hasAlpha = true;
-    textplaneTexture.drawText("(0, 0, 0)",0,140,"bold 50px monospace","blue","white",true,true);
+    textplaneTexture.drawText("(0,0,0)",0,140,"bold 55px monospace","blue","white",true,true);
     
 
     // Register a render loop to repeatedly render the scene
@@ -69,10 +67,15 @@ function init(){
     // Set initial destination
     vx = 100, vy = 100, vz = -300;
 
+    // Set initial starting point
+    px = 0, py = 0, pz = 0;
+
     // Set transformCo_ordinate
     transformCo_ordinate = false;
 
-    
+    // Add text to the manual
+    var manual = document.getElementById("manual").getElementsByTagName("p");
+    manual[0].innerHTML = "<b><u>Translation</u></b><br/><i><u>Introduction to the interface :</u></i><br/>This experiment is to demonstrate how translation transformation works on point and coordinate system. The point and coordinate system is displayed in the top part. The point starts at the origin. The current coordinates of the point are displayed above the point as well as below. Below the coordinates is the checkbox to select whether to move the point or the coordinate system. Below it is the transformation matrix applied for translation. Below it is the slider to move the point or the coordinate system to the current destination. Than there are input for X, Y, Z. The <b>Set New Destination</b> button sets the X,Y,Z values as the current destination. The <b>Set Starting Point</b> changes the starting position of the point to the value of X, Y, Z. The <b>Reset Camera</b> button resets the camera of the display. ";
 
     displayTransformationMatrix();
     displayPosition();
@@ -91,16 +94,16 @@ function displayPosition()
 {
     var info = document.getElementById("point_info");
     var sliderValue = document.getElementById("slider").getElementsByTagName("input")[0].value;
-    var coordinate_info = "(" + (vx/100)*sliderValue + ", " + (vy/100)*sliderValue + ", " + (vz/100)*sliderValue + ")"; 
-    info.getElementsByTagName("p")[0].innerHTML = coordinate_info;
+    var coordinate_info = "(" + (((vx-px)/100)*sliderValue + px) + "," + (((vy-py)/100)*sliderValue + py) + "," + (((vz-pz)/100)*sliderValue + pz) + ")"; 
+    info.getElementsByTagName("p")[3].innerHTML = coordinate_info;
     
 }
 
 function setNewDestination()
 {
-    vx = document.getElementById("newX").value;
-    vy = document.getElementById("newY").value;
-    vz = document.getElementById("newZ").value;
+    vx = parseFloat(document.getElementById("destX").value);
+    vy = parseFloat(document.getElementById("destY").value);
+    vz = parseFloat(document.getElementById("destZ").value);
     resetPoint();
     displayTransformationMatrix();
     displayPosition();
@@ -110,14 +113,13 @@ function setNewDestination()
 function changePosition(newVal)
 {
     if(!transformCo_ordinate) {
-        sphere.position = new BABYLON.Vector3((vx/100)*newVal,(vy/100)*newVal,(vz/100)*newVal);
+        sphere.position = new BABYLON.Vector3(((vx-px)/100)*newVal + px,((vy-py)/100)*newVal + py,((vz-pz)/100)*newVal + pz);
+        console.log(sphere.position);
     }
     else{
         Xaxis =  BABYLON.MeshBuilder.CreateLines("Xaxis",{points:[new BABYLON.Vector3(1000,(vy/100)*newVal,(vz/100)*newVal),new BABYLON.Vector3(-1000,(vy/100)*newVal,(vz/100)*newVal)], instance:Xaxis});
         Yaxis =  BABYLON.MeshBuilder.CreateLines("Yaxis",{points:[new BABYLON.Vector3((vx/100)*newVal,1000,(vz/100)*newVal),new BABYLON.Vector3((vx/100)*newVal,-1000,(vz/100)*newVal)], instance:Yaxis});
-        Zaxis =  BABYLON.MeshBuilder.CreateLines("Zaxis",{points:[new BABYLON.Vector3((vx/100)*newVal,(vy/100)*newVal,1000),new BABYLON.Vector3((vx/100)*newVal,(vy/100)*newVal,-1000)], instance:Zaxis});
-
-        
+        Zaxis =  BABYLON.MeshBuilder.CreateLines("Zaxis",{points:[new BABYLON.Vector3((vx/100)*newVal,(vy/100)*newVal,1000),new BABYLON.Vector3((vx/100)*newVal,(vy/100)*newVal,-1000)], instance:Zaxis});        
     }
     updateCoordinates();
     displayPosition();
@@ -142,21 +144,27 @@ function resetPoint()
     }
     else
     {
-        sphere.position = new BABYLON.Vector3(0,0,0);
+        sphere.position = new BABYLON.Vector3(px,py,pz);
     }
     document.getElementById("slider").getElementsByTagName("input")[0].value = 0;
     updateCoordinates();
-    
 }
 
 function updateCoordinates()
 {
     var sliderValue = document.getElementById("slider").getElementsByTagName("input")[0].value;
-    var coordinate_info = "(" + (vx/100)*sliderValue + ", " + (vy/100)*sliderValue + ", " + (vz/100)*sliderValue + ")"; 
-    textplaneTexture.drawText(coordinate_info,0,140,"bold 50px monospace","blue","white",true,true);
+    var coordinate_info = "(" + (((vx-px)/100)*sliderValue + px) + "," + (((vy-py)/100)*sliderValue + py) + "," + (((vz-pz)/100)*sliderValue + pz) + ")"; 
+    textplaneTexture.drawText(coordinate_info,0,140,"bold 55px monospace","blue","white",true,true);
 }
 function resetCamera()
 {
-    camera.setPosition(new BABYLON.Vector3(100,100,100));
-   // camera.alpha = camera.beta = Math.PI / 2;
+    camera.setPosition(new BABYLON.Vector3(300,300,300));
+}
+
+function changeStartingPoint()
+{
+    px = parseFloat(document.getElementById("strtX").value);
+    py = parseFloat(document.getElementById("strtY").value);
+    pz = parseFloat(document.getElementById("strtZ").value);
+    resetPoint();
 }
